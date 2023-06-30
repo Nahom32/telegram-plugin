79369f282292acd5c1453c68fea4b7505ed2c4f0
Nahom32 <senaynahom00@gmail.com>
2023-06-30 09:49:38 +0300

    first commit

---
+++ models/telegram_group.py
@@ -0,0 +1,4 @@
+class Telegram_Group:
+    def __init__(self, name):
+        self.name = name
+    
---
+++ models/telegram_user.py
@@ -0,0 +1,4 @@
+class Telegram_User:
+    def __init__(self,name):
+        self.name = name
+    
